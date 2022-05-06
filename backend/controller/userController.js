75bf989dd0394bd0efc39e0231c8d52d61b04acb
Sandro Mejia <sandromejia3409@gmail.com>
2022-05-06 00:05:58 -0400

    added get user data route and created frontend folder

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const User = require("../model/userModel");
 
@@ -7,7 +8,9 @@ const registerUser = async (req, res) => {
     const { fname, lname, email, password } = req.body;
 
     if (!fname || !lname || !email || !password) {
-      res.status(400);
+      res.status(400).json({
+        message: "Please enter all fields",
+      });
       throw new Error("Please enter all fields");
     }
 
@@ -15,14 +18,20 @@ const registerUser = async (req, res) => {
     const userExist = await User.findOne({ email });
 
     if (userExist) {
-      res.status(400);
+      res.status(400).json({
+        message: "user already exits",
+      });
       throw new Error("User already exists");
     }
+
+    console.log("register is contiuing");
 
     //hash password
     //encrypts password user enters
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log("hashed passed");
 
     const user = await User.create({
       fname,
@@ -31,23 +40,64 @@ const registerUser = async (req, res) => {
       password: hashedPassword,
     });
 
+    console.log("created passed");
+
     if (user) {
       res.status(200).json({
-        _id: user.id,
+        _id: user._id,
         firstName: user.fname,
         lastName: user.lname,
         email: user.email,
         token: generateToken(user._id),
       });
+    } else {
+      res.status(400).json({
+        message: "Invalid data",
+      });
+      console.log("error");
+      throw new Error("invalid user data");
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //checks database if email exists
+    const user = await User.findOne({ email });
+
+    /**
+     * if the users exists
+     * it compares the password the user input to the real account password
+     */
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.status(201).json({
+        _id: user.id,
+        fname: user.fname,
+        lname: user.lname,
+        token: generateToken(user.id),
+      });
+    } else {
+      res.status(400);
+      res.json("invalid user");
+      throw new Error("invalid user data");
+    }
+  } catch (error) {
+    res.status(400).json({
+      login: false,
+    });
+  }
 };
 
 //Generate JWT
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, "abc1234", {
     expiresIn: "30d",
   });
 };
 
-module.exports = { registerUser };
+module.exports = { registerUser, getUser };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controller/userController");
+const { registerUser, getUser } = require("../controller/userController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", registerUser);
+router.get("/login", getUser);
 
 module.exports = router;
